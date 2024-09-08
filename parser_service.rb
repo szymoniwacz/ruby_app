b3f1abc042b6e7fa40b13f76824d8160771d87a4
Szymon Iwacz <szymon@iwacz.pl>
2024-09-08 12:35:29 +0200

    Create initial ParserService class with placeholder methods

---
+++ parser_service.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ParserService
+  def initialize(file)
+    @file = file
+  end
+
+  def page_views
+    []
+  end
+
+  def unique_page_views
+    []
+  end
+end
