e1324f8d0803fedc4f49fd2281a7176a399ed474
Szymon Iwacz <szymon@iwacz.pl>
2024-09-08 14:23:09 +0200

    Implement page views counting in ParserService

--- parser_service.rb
+++ parser_service.rb
@@ -7,10 +7,33 @@ class ParserService
   end
 
   def page_views
-    []
+    views = Hash.new(0)
+
+    each_line do |line|
+      page, _ip = parse_line(line)
+      views[page] += 1 if page
+    end
+
+    sorted_views(views)
   end
 
   def unique_page_views
     []
+  end
+
+  private
+
+  def each_line(&)
+    File.readlines(@file).each(&)
+  end
+
+  def parse_line(line)
+    page, ip = line.split
+
+    [page, ip]
+  end
+
+  def sorted_views(views)
+    views.sort_by { |page, count| [-count, page] }
   end
 end
